9ce4200fbbadca1ec2c420df0bf07664e6191e7b
Md Antor Mia <mdantormia1779@gmail.com>
2026-06-24 17:45:06 +0600

    some done

--- index.js
+++ index.js
@@ -12,12 +12,18 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
+// app.use(
+//   cors({
+//     origin: process.env.NEXT_PUBLIC_URL,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     credentials: true,
+//   }),
+// );
 app.use(
   cors({
-    origin: process.env.NEXT_PUBLIC_URL,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    origin: true,
     credentials: true,
-  }),
+  })
 );
 
 // MongoDB URI
